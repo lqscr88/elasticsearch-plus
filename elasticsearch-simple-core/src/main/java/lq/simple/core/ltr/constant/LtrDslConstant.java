7eda763492f07a80594c92c747dab8671ab756f9
lqscr88 <62920348+lqscr88@users.noreply.github.com>
2023-09-20 15:55:33 +0800

    Delete elasticsearch-simple-core directory

--- elasticsearch-simple-core/src/main/java/lq/simple/core/ltr/constant/LtrDslConstant.java
+++
@@ -1,5 +0,0 @@
-package lq.simple.core.ltr.constant;
-
-public class LtrDslConstant {
-    public static final  String  LTR_QUERY_DSL = "{\"rescore\":{\"window_size\":1000,\"query\":{\"rescore_query\":{\"sltr\":{\"params\":{\"${kw}\":\"${value}\"},\"model\":\"${modelName}\",\"active_features\":[\"${TemplateName}\"]}}}}}";
-}
--- elasticsearch-simple-core/src/main/java/lq/simple/util/CharacterUtils.java
+++
@@ -1,9 +0,0 @@
-package lq.simple.util;
-
-public class CharacterUtils {
-
-
-    public final static String SLASH = "/";
-    public final static String TERM = " ";
-    public final static String COMMA = ",";
-}
